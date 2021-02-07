66d49ab2383f952bf3848d9b539340cd3aa0b687
skeptikantin <suz@extraflach.de>
2021-02-07 11:40:03 +0100

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -127,16 +127,16 @@ Template("training.csv", row =>
             .sort(()=>Math.random()-Math.random())
         ,
         newText("Alt1", alts[0])
-            .print("center at 0%", "top at 50%", getCanvas("container"))
+            .print("center at 0%", "top at 20%", getCanvas("container"))
         ,
         newText("Alt2", alts[1])
-            .print("center at 100%", "top at 50%", getCanvas("container"))
+            .print("center at 100%", "top at 20%", getCanvas("container"))
         ,
         newScale("slider", 100)
             .slider()
             .size("500px", "1em")
             .css("max-width", "unset")
-            .print(0, "bottom at 70%", getCanvas("container"))
+            .print(0, "bottom at 50%", getCanvas("container"))
             .log()
             .wait()
         ,
